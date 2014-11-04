92353de8e8c23b8877f98024bec2f4c251e7d93b
Binoj Daniel <councilsoft@gmail.com>
2014-11-04 18:57:50 -0500

    Added missing file AlertManager.cs

---
+++ src/CouncilSoft.BootstrapAlert_Solution/CouncilSoft.BootstrapAlert/AlertManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace CouncilSoft.BootstrapAlert
+{
+    /// <summary>
+    /// Static utility class for managing client side status messages.
+    /// </summary>
+    public static class AlertManager
+    {
+        /// <summary>
+        /// Sets the alert for the mvc view to render. Rendered by Html.RenderAlertMessages().
+        /// </summary>
+        /// <param name="controllerBase"></param>
+        /// <param name="severity"></param>
+        /// <param name="alertMessage"></param>
+        public static void SetAlert(ControllerBase controllerBase, AlertSeverity severity, String alertMessage)
+        {
+            if (controllerBase == null)
+                throw new ArgumentNullException("controllerBase");
+            if (alertMessage == null)
+                throw new ArgumentNullException("alertMessage");
+
+            controllerBase.TempData["alertType"] = (Int32)severity;
+            controllerBase.TempData["alertMessage"] = alertMessage;
+        }
+
+        /// <summary>
+        /// Removes the alert from being displayed in the UI.
+        /// </summary>
+        /// <param name="controllerBase"></param>
+        public static void ClearAlert(ControllerBase controllerBase)
+        {
+            if (controllerBase == null)
+                throw new ArgumentNullException("controllerBase");
+
+            controllerBase.TempData["alertType"] = null;
+            controllerBase.TempData["alertMessage"] = null;
+        }
+    }
+}
